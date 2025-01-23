80869813e66cf9886940e4e6a650aa0e510c95f4
Asma-ali22 <ma5144236@gmail.com>
2025-01-23 20:33:43 +0500

    my project

--- src/sanity/schemaTypes/order.ts
+++ src/sanity/schemaTypes/order.ts
@@ -1,58 +1,4 @@
-// sanity/schemas/order.js
-import { defineType } from "sanity"
-// export default  {
-//     name: 'order',
-//     type: 'document',
-//     title: 'Order',
-//     fields: [
-//       {
-//         name: 'customerName',
-//         type: 'string',
-//         title: 'Customer Name',
-//       },
-//       {
-//         name: 'customerEmail',
-//         type: 'string',
-//         title: 'Customer Email',
-//       },
-//       {
-//         name: 'customerPhone',
-//         type: 'string',
-//         title: 'Customer Phone',
-//       },
-//       {
-//         name: 'customerAddress',
-//         type: 'string',
-//         title: 'Customer Address',
-//       },
-//       {
-//         name: 'items',
-//         type: 'array',
-//         title: 'Items',
-//         of: [
-//           {
-//             type: 'object',
-//             fields: [
-//               {
-//                 name: 'product',
-//                 type: 'reference',
-//                 to: [{ type: 'product' }],
-//               },
-//               {
-//                 name: 'quantity',
-//                 type: 'number',
-//               },
-//             ],
-//           },
-//         ],
-//       },
-//       {
-//         name: 'total',
-//         type: 'number',
-//         title: 'Total Price',
-//       },
-//     ],
-//   };
+
 const orderSchema = {
   name: 'order',
   type: 'document',
@@ -69,7 +15,7 @@ const orderSchema = {
           type: 'string',
           title: 'Customer Email',
           description: 'The email address of the customer for order confirmation and communication.',
-          validation: (Rule: any) => Rule.required().email(),
+          validation: (Rule: any) => Rule.required().email()
       },
       {
           name: 'customerPhone',

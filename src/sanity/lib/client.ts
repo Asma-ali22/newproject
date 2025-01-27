7a7e87f30eb528e98b114464be19d885ab195696
Asma-ali22 <ma5144236@gmail.com>
2025-01-27 15:23:56 +0500

    my project

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -15,8 +15,10 @@
   import { createClient } from '@sanity/client';
   
   const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: 'pf1qb9pc',
+    dataset: 'production',
+    apiVersion:'2025-01-13',
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN='skETmoy89cs80Zx5WKfksp8SDERMWJuwLpQG3i9IdlLK9B18ndqemI6RyPmzIM0HxU5osUkVLB8j2OAP13XPfiSor9bNSDr3ECnYDwFPweF4fxsGKp4agr1ewvYdlbG5lw6WWMxrNbuto6Kea9Hkfw9aOQxbP23zU36zcXW3F2eX4gCBhGlq',
     useCdn: true,
   });
   
